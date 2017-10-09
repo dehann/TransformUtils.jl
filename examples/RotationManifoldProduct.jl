edaa3da20fe59a4530faa42ee1a99ded241eab4c
dehann <dehann.fourie@gmail.com>
2017-10-09 00:16:02 -0400

    first example running

--- examples/RotationManifoldProduct.jl
+++ examples/RotationManifoldProduct.jl
@@ -15,24 +15,34 @@ q2 = convert(Quaternion, so3(randn(3)))
 
 function weightedmean(
             qArrMu::Vector{Quaternion};
-            covariances::Vector{Array{Float64,2}}=Vector{Array{Float64,2}}(0),
+            covariances::Vector{Array{Float64,2}}=Vector{Array{Float64,2}}(),
             tol::Float64=1e-7,
             allowediters::Int=30  )
   #
   covariances = length(covariances) == length(qArrMu) ? covariances : [eye(3) for i in 1:length(qArrMu)]
-  dq = deepcopy(qArr[1])
+  muq = deepcopy(qArrMu[1])
   S = zeros(3)
-  i = 0
+  invC = zeros(3,3)
   epsi = 1.0
+  i = 0
   while epsi > tol && i < allowediters
     i+=1
+    j=0
     S[:] = 0.0
-    for q in qArr
-      S[:] += covariances[i] \ deltaso3vee(dq, q)
+    invC[:] = 0.0
+    for q in qArrMu
+      j += 1
+      invCov = covariances[j] \ eye(3)
+      invC += invCov
+      S[:] += invC * deltaso3vee(muq, q)
     end
+    dq = invC \ S
+    newmuq = muq * so3(dq)
+    epsi = abs(muq.s - newmuq.s) + norm(muq.v - newmuq.v)
+    muq = newmuq
   end
 
-  return 
+  return muq
 end
 # a = q1
 # b = q2
@@ -45,5 +55,10 @@ end
 # c = d
 
 Vector{Array{Float64,2}}(0)
+
+q2
+
+weightedmean([q1,q2])
+
 
 #
