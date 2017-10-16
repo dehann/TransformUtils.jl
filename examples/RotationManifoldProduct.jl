12adff913fdab54481526fe88f0c9ea7094a3ce8
dehann <dehann.fourie@gmail.com>
2017-10-16 06:35:25 -0400

    work in progress

--- examples/RotationManifoldProduct.jl
+++ examples/RotationManifoldProduct.jl
@@ -2,22 +2,48 @@
 
 using TransformUtils
 
+import Base: convert
 
 # means = zeros(4,2)
 # means[1,:] = 1.0
 
-sigmas = rand(1,2)
-sigmas = ones(1,2)
 
-q1 = Quaternion(0)
-q2 = convert(Quaternion, so3(randn(3)))
+using DrakeVisualizer, GeometryTypes, Colors, CoordinateTransformations, Rotations, StaticArrays
+
+
+DrakeVisualizer.any_open_windows() || DrakeVisualizer.new_window();
+
+vis = Visualizer()
+
+
+function drawquat!(vis::DrakeVisualizer.Visualizer,
+            q::Rotations.Quat,
+            qsym::Symbol;
+            length=1.,
+            radius=0.01,
+            color=RGBA(1.,0,0,0.5) )
+    #
+    v = SVector(length,0,0)
+    line = PolyLine([SVector(0.,0,0), LinearMap(q)(v)], radius=radius)
+    linedata = GeometryData(line, color)
+    setgeometry!(vis[qsym], linedata)
+    nothing
+end
+
+
+
+function convert(::Type{Rotations.Quat}, q::TransformUtils.Quaternion)
+    Rotations.Quat(q.s, q.v...)
+end
+
 
 
 function weightedmean(
             qArrMu::Vector{Quaternion};
             covariances::Vector{Array{Float64,2}}=Vector{Array{Float64,2}}(),
             tol::Float64=1e-7,
-            allowediters::Int=30  )
+            allowediters::Int=30,
+            docovariance::Bool=false  )
   #
   covariances = length(covariances) == length(qArrMu) ? covariances : [eye(3) for i in 1:length(qArrMu)]
   muq = deepcopy(qArrMu[1])
@@ -26,7 +52,7 @@ function weightedmean(
   epsi = 1.0
   i = 0
   while epsi > tol && i < allowediters
-    i+=1
+    @show i+=1
     j=0
     S[:] = 0.0
     invC[:] = 0.0
@@ -34,31 +60,58 @@ function weightedmean(
       j += 1
       invCov = covariances[j] \ eye(3)
       invC += invCov
-      S[:] += invC * deltaso3vee(muq, q)
+      S[:] +=  deltaso3vee(muq, q) #invC *
     end
-    dq = invC \ S
+    dq = 0.5*S #invC \ S
     newmuq = muq * so3(dq)
     epsi = abs(muq.s - newmuq.s) + norm(muq.v - newmuq.v)
     muq = newmuq
   end
 
-  return muq
+  if docovariance
+      totCov = invC \ eye(3)
+      return muq, totCov
+  else
+      return muq
+  end
 end
-# a = q1
-# b = q2
-# c = deepcopy(a)
-# Sctoa = deltaso3vee(c, a)
-# Sctob = deltaso3vee(c, b)
-# S = 0.5*(Sctoa + Sctob)
-# d = c * so3(S)
-# epsi = abs(c.s - d.s) + norm(c.v-d.v)
-# c = d
+a = q1
+b = q2
+c = deepcopy(a)
+Sctoa = deltaso3vee(c, a)
+Sctob = deltaso3vee(c, b)
+S = 0.5*(Sctoa + Sctob)
+d = c * so3(S)
+epsi = abs(c.s - d.s) + norm(c.v-d.v)
+c = d
 
-Vector{Array{Float64,2}}(0)
 
-q2
+sigmas = rand(1,2)
+sigmas = ones(1,2)
 
-weightedmean([q1,q2])
+q1 = Quaternion(0)
+q2 = convert(Quaternion, so3(randn(3)))
+
+
+q1s = convert(Rotations.Quat, q1)
+q2s = convert(Rotations.Quat, q2)
+
+
+q12 = weightedmean([q1,q2], docovariance=true)
+q12s = convert(Rotations.Quat, q12[1])
+
+
+drawquat!(vis, q1s, :q1)
+drawquat!(vis, q2s, :q2, color=RGBA(0,1.,0,0.5))
+
+drawquat!(vis, q12s, :q12, color=RGBA(1.,1.,0,0.5))
+
+
+q12t = convert(Rotations.Quat, d)
+drawquat!(vis, q12t, :q12t, color=RGBA(0.,0.,1,0.5))
+
+
+
 
 
 #
